c7bf113005decd4d5580e47eff764bc5258bb085
VSR <vsr7@live.com>
2021-07-13 00:04:16 +0530

    Updated bash script for copy files and output

--- firstbashscript.sh
+++ firstbashscript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ########<<<<Welcome to my Script>>>############
 firstline=$(head -1 ./source/changelog.md)
-echo $firstline
+#echo $firstline
 
 read -a splitfirstline <<< $firstline
-
+#using -a will convert the string to arrays.
 
 version=${splitfirstline[1]}
 echo -e "You are using Version $version \nDo you want to continue with this Version?";
@@ -13,12 +13,39 @@ read versioncontinue
 
 if [ $versioncontinue == 1 ]
   then
-     listed=$(ls ./source) 
-     echo "$listed"
+     for listed in `ls ./source`
+     # alternatively ->
+     # for listed in $(ls ./source)
+     do 
+    
+    
+     if [ $listed == "secretinfo.md" ]
+      then 
+      echo "Sorry! file $listed will not be copied!"
+       else echo 'Copying' $listed
+      (cp -p ./source/$listed ./build/) 
+      fi
+     done;
+     echo "Do you want to see the files in the destination? (Y/N)"
+     read yon
+     if [ ${yon^^} == Y ]
+#use '${Y^^}' for convert all CAPS.
+#use  '${Y,,}' for convert all lowercase.  
+     then echo "$(cd build && ls)" 
+#using "double quotes" avoids 'field-splitting'
+
+#"$(cd build ; ls -lr)"  --- alternative
+      
+      elif [ $yon == N ]
+      then echo "Okay"
+# use : (colon) for - do nothing after else/elif 
+      else
+      echo "Invalid input!"
+      fi   
 else
-     echo "comeback when you're ready!"
+     echo "comeback when you're ready"
 fi
 
-
+echo -e "Build version $version contains the files,\n $(ls ./build/) "
 
 ##this script tells you the version of a program by taking in the input from a file.
